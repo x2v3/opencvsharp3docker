fad36ec9b756632673dad0d1306c1e7bb129f594
jx wang <jx@x2v3.com>
2018-03-30 13:55:29 +0800

    add libgdiplus

--- buildopencv.sh
+++ buildopencv.sh
@@ -13,7 +13,7 @@ mv opencv_contrib-3.4.1 opencv_contrib
 wget https://github.com/shimat/opencvsharp/archive/3.4.1.20180320.tar.gz -O opencvsharp.tar.gz
 tar -zxvf opencvsharp.tar.gz
 mv opencvsharp-3.4.1.20180320 opencvsharp
-apt-get install --no-install-recommends -y libopencv-dev yasm libjpeg-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils pkg-config
+apt-get install --no-install-recommends -y libopencv-dev yasm libjpeg-dev libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev libv4l-dev python-dev python-numpy libtbb-dev libqt4-dev libgtk2.0-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils pkg-config libgdiplus
 apt-get install --no-install-recommends  -y curl build-essential checkinstall cmake unzip libunwind8 gettext wget git
 # build opencv
 cd /opencv/opencv 
